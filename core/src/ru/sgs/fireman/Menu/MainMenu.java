4c081b644af6564bcf9907b3e32c1d9d3998846c
Unknown <yarikbur@Mail.ru>
2019-07-03 15:32:43 +0300

    Add background for main menu

---
+++ core/src/ru/sgs/fireman/Menu/MainMenu.java
@@ -0,0 +1,114 @@
+package ru.sgs.fireman.Menu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class MainMenu implements Screen {
+
+    private SpriteBatch batch;
+    private Texture logo;
+    private ShapeRenderer shape;
+    private int widthWindow;
+    private int heightWindow;
+
+    private final float speedBackground = 1;
+    private Sprite background[][] = new Sprite[6][2];
+    private Texture sun;
+
+    public MainMenu(SpriteBatch batch){
+        this.batch = batch;
+        shape = new ShapeRenderer();
+
+        logo = new Texture("FireManLogo.png");
+        sun = new Texture("background/sun.png");
+
+        widthWindow = Gdx.app.getGraphics().getWidth();
+        heightWindow = Gdx.app.getGraphics().getHeight();
+
+        for (int i=0; i<6; i++){
+            background[i][0] = new Sprite(new Texture("background/bg" + (i+1) + ".png"));
+            background[i][1] = new Sprite(new Texture("background/bg" + (i+1) + ".png"));
+            background[i][0].setX(0);
+            background[i][1].setX(background[i][0].getWidth());
+        }
+    }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Градиент
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        shape.rect(0, 0,widthWindow, heightWindow,
+                new Color(245/255f, 220/255f, 100/255f, 1), new Color(245/255f, 220/255f, 100/255f, 1),
+                new Color(220/255f, 150/255f, 90/255f, 1), new Color(220/255f, 150/255f, 90/255f, 1));
+        shape.end();
+
+        batch.begin();
+
+        //Фон
+        batch.draw(sun, widthWindow/2+widthWindow/4, heightWindow/2+heightWindow/4);
+        for (int i=0; i<6; i++){
+            background[i][0].draw(batch);
+            background[i][1].draw(batch);
+
+            background[i][0].setX(background[i][0].getX()-speedBackground*(i+0.5f));
+            background[i][1].setX(background[i][1].getX()-speedBackground*(i+0.5f));
+
+            if(background[i][0].getX() <= -background[i][0].getWidth())
+                background[i][0].setX(background[i][1].getX()+background[i][1].getWidth());
+
+            if(background[i][1].getX() <= -background[i][1].getWidth())
+                background[i][1].setX(background[i][0].getX()+background[i][0].getWidth());
+        }
+
+        //Лого FireMan
+        batch.draw(logo, ((widthWindow/2)-(logo.getWidth()/2)), ((heightWindow/2)+logo.getHeight()));
+
+        //Кнопки
+
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose(){
+        batch.dispose();
+        logo.dispose();
+    }
+
+    @Override
+    public void show(){
+
+    }
+
+    @Override
+    public void hide(){
+
+    }
+
+    @Override
+    public void pause(){
+
+    }
+
+    @Override
+    public void resume(){
+
+    }
+
+    @Override
+    public void resize(int width, int height){
+        widthWindow = width;
+        heightWindow = height;
+
+        System.out.println("Window width: " + width + " and height: " + height);
+    }
+}
--- desktop/src/ru/sgs/fireman/desktop/DesktopLauncher.java
+++ desktop/src/ru/sgs/fireman/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import ru.sgs.fireman.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.title = "FireMan";
 		new LwjglApplication(new Main(), config);
 	}
 }
